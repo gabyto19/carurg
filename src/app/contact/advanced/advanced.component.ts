178fd97e59248a316dfa33d22622ce375816ce87
gabyto19 <gabuniagoga19@gmail.com>
2024-07-30 18:27:19 +0400

    Advanced Message functionality has been completed

--- src/app/contact/advanced/advanced.component.ts
+++ src/app/contact/advanced/advanced.component.ts
@@ -1,10 +1,46 @@
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-advanced',
+//   templateUrl: './advanced.component.html',
+//   styleUrl: './advanced.component.css'
+// })
+// export class AdvancedComponent {
+
+// }
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-advanced',
   templateUrl: './advanced.component.html',
-  styleUrl: './advanced.component.css'
+  styleUrls: ['./advanced.component.css']
 })
 export class AdvancedComponent {
+  carForm: FormGroup;
+  successMessage: string;
 
+  constructor(private fb: FormBuilder, private http: HttpClient) {
+    this.carForm = this.fb.group({
+      brand: ['', Validators.required],
+      model: ['', Validators.required],
+      year: ['', [Validators.required, Validators.pattern('^[0-9]{4}$')]],
+      details: ['', Validators.required],
+      telephone: ['', [Validators.required, Validators.pattern('^[0-9-]+$')]]
+    });
+    this.successMessage = '';
+  }
+
+  onSubmit() {
+    if (this.carForm.valid) {
+      this.http.post('/api/send-car-details', this.carForm.value).subscribe(response => {
+        console.log('Response:', response);
+        this.successMessage = 'Your details have been sent';
+      }, error => {
+        console.error('Error sending details', error);
+        this.successMessage = '';  // Clear the message if there was an error
+      });
+    }
+  }
 }
